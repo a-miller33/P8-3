37631b6f2ed82bc8da3ae50c4cb5e215ed12e1dc
Adam Miller <a-miller.33@onu.edu>
2024-10-24 14:20:28 -0400

    completed romanCharValue function

--- P8-3/P8-3.cpp
+++ P8-3/P8-3.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int romanCharValue(char r);
+
+int main() {
+	char r;
+	cout << "Enter Roman number: ";
+	cin >> r;
+	cout << r << " = " << romanCharValue(r) << endl;
+
+	return 0;
+}
+
+int romanCharValue(char r) {
+	if (r == 'I') return 1;
+	else if (r == 'V') return 5;
+	else if (r == 'X') return 10;
+	else if (r == 'L') return 50;
+	else if (r == 'C') return 100;
+	else if (r == 'D') return 500;
+	else if (r == 'M') return 1000;
+}
