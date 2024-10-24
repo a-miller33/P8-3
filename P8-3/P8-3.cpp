3e4aa3b339f180e1ea5494bdb9f62d8102928869
Adam Miller <a-miller.33@onu.edu>
2024-10-24 14:29:56 -0400

    completed convertRomanToInt function

--- P8-3/P8-3.cpp
+++ P8-3/P8-3.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 int romanCharValue(char r);
+int convertRomanToInt(string s);
 
 int main() {
-	char r;
+	string s;
 	cout << "Enter Roman number: ";
-	cin >> r;
-	cout << r << " = " << romanCharValue(r) << endl;
+	cin >> s;
+	cout << s << " = " << convertRomanToInt(s) << endl;
 
 	return 0;
 }
@@ -21,4 +22,19 @@ int romanCharValue(char r) {
 	else if (r == 'C') return 100;
 	else if (r == 'D') return 500;
 	else if (r == 'M') return 1000;
+}
+
+int convertRomanToInt(string s) {
+	int total = 0;
+	while (s != "") {
+		if (romanCharValue(s[0]) >= romanCharValue(s[1]) || s.length() == 1) {
+			total += romanCharValue(s[0]);
+			s.erase(0, 1);
+		}
+		else {
+			total += (romanCharValue(s[1]) - romanCharValue(s[0]));
+			s.erase(0, 2);
+		}
+	}
+	return total;
 }
